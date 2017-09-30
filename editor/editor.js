4526ce3b6084d98989104dbb5b2e53244c203692
Windows87 <yuri.yurifaria@gmail.com>
2017-09-30 20:33:56 -0300

    Write and Image Pen added

--- editor/editor.js
+++ editor/editor.js
@@ -3,13 +3,16 @@ var remote = require('electron').remote;
 var dialog = remote.dialog;
 var BrowserWindow = remote.BrowserWindow;
 var path = require('path');
+var Jimp = require("jimp");
 
 var view = new View;
 var tool = 'pixel-pen';
 
 var font = new Font;
 font = font.alphabet;
-var fontChoose = 'MARACANA';
+var wordWritePen = 'abc';
+
+var imagePen = new Array;
 
 document.addEventListener('DOMContentLoaded', function(){
 	var file = window.location.href.split('?file=')[1];
@@ -120,6 +123,176 @@ document.addEventListener('DOMContentLoaded', function(){
 
 	document.getElementById('write-pen').addEventListener('click', function(){
 		tool = 'write-pen';
+		document.getElementById('tool-background').style['display'] = "flex";
+
+		var toolMenuConfig = document.getElementById('tool-menu--config');
+		toolMenuConfig.innerHTML = '';
+		
+		var wordDiv = document.createElement('div');
+		var wordInput = document.createElement('input');
+		var wordLabel = document.createElement('label');
+		var wordButton = document.createElement('button');
+
+		wordDiv.classList.add('mdl-textfield');
+		wordDiv.classList.add('mdl-js-textfield');
+		
+		wordInput.classList.add('mdl-textfield__input');
+		wordInput.id = 'tool-menu--config__word';
+		
+		wordLabel.classList.add('mdl-textfield__label');
+		wordLabel.htmlFor = 'tool-menu--config__word';
+		wordLabel.innerText = 'Write the Word';
+
+		wordButton.classList.add('mdl-button');
+		wordButton.innerText = 'Send';
+		wordButton.addEventListener('click', function(){
+			wordWritePen = wordInput.value;
+			document.getElementById('tool-background').style['display'] = 'none';
+		});
+
+
+		wordDiv.appendChild(wordInput);
+		wordDiv.appendChild(wordLabel);
+		toolMenuConfig.appendChild(wordDiv);
+		toolMenuConfig.appendChild(document.createElement('br'));
+		toolMenuConfig.appendChild(wordButton);
+		componentHandler.upgradeDom();
+	});
+
+	document.getElementById('image-pen').addEventListener('click', function(){
+		tool = 'image-pen';
+		document.getElementById('tool-background').style['display'] = "flex";
+
+		var toolMenuConfig = document.getElementById('tool-menu--config');
+		toolMenuConfig.innerHTML = '';
+		
+		var imgFile = document.createElement('img');
+		var imgButton = document.createElement('button');
+
+		var constFirst = 64;
+		var constTwo = 32;
+
+		var filename;
+
+		imgFile.style['width'] = '128px';
+		imgFile.style['height'] = '64px';
+		imgFile.style['background-color'] = '#000000';
+		imgFile.addEventListener('click', function(){
+			dialog.showOpenDialog({title: "Select Image"}, (fileName) => {
+		    	if(fileName === undefined){
+		        	return;
+		    	}
+
+		    	filename = fileName.toString().replace(new RegExp("/", 'g'), '//');
+				Jimp.read(filename).then(function (lenna) {
+					constFirst = (lenna.bitmap.width / lenna.bitmap.height) * constTwo;
+				    lenna.resize(constFirst, constTwo)
+				    	 .quality(60)
+				         .greyscale()
+				  	     .contrast(1)
+				         .getBase64(Jimp.MIME_JPEG, function (err, src) {
+				         	  imgXInput.value = parseInt(constFirst);
+				         	  imgYInput.value = constTwo;
+				         	  imgFile.style['width'] = (constFirst * 2).toString() + 'px';
+				         	  imgFile.style['height'] = (constTwo * 2).toString() + 'px';
+				              imgFile.src = src;
+				         });
+				}).catch(function (err) {
+				    console.error(err);
+				});
+			});
+		});
+		
+		function readIt(x, y){
+			Jimp.read(filename).then(function (lenna) {
+				lenna.resize(x, y)
+				    .quality(60)
+				    .greyscale()
+				  	.contrast(1)
+				    .getBase64(Jimp.MIME_JPEG, function (err, src) {
+				    	constFirst = x;
+				    	constTwo = y;
+				        imgFile.style['width'] = (x * 2).toString() + 'px';
+				        imgFile.style['height'] = (y * 2).toString() + 'px';
+				        imgFile.src = src;
+				    });
+			}).catch(function (err) {
+				console.error(err);
+			});			
+		}
+
+		var imgXDiv = document.createElement('div');
+		var imgXInput = document.createElement('input');
+		imgXInput.classList.add('mdl-textfield__input');
+		imgXInput.addEventListener('change', function(){
+			if(parseInt(this.value) > 64){
+				this.value = 64;
+			}
+			readIt(parseInt(this.value), constTwo);
+		});
+		imgXDiv.classList.add('mdl-textfield');
+		imgXDiv.classList.add('mdl-js-textfield');
+		imgXDiv.style['width'] = '25px';
+		imgXDiv.appendChild(imgXInput);
+
+		var imgYDiv = document.createElement('div');
+		var imgYInput = document.createElement('input');
+		imgYInput.classList.add('mdl-textfield__input');
+		imgYInput.addEventListener('change', function(){
+			if(parseInt(this.value) > 32){
+				this.value = 32;
+			}			
+			readIt(constFirst, parseInt(this.value));
+		});
+		imgYDiv.classList.add('mdl-textfield');
+		imgYDiv.classList.add('mdl-js-textfield');
+		imgYDiv.style['width'] = '25px';
+		imgYDiv.appendChild(imgYInput);
+
+		var imgsDiv = document.createElement('div');
+		var xSpan = document.createElement('span');
+		xSpan.innerText = 'x';
+		imgsDiv.appendChild(imgXDiv);
+		imgsDiv.appendChild(xSpan);
+		imgsDiv.appendChild(imgYDiv);
+
+		imgButton.classList.add('mdl-button');
+		imgButton.innerText = 'Send';
+		imgButton.addEventListener('click', function(){
+			imagePen = new Array;
+
+			Jimp.read(filename).then(function (lenna) {
+				constFirst = (lenna.bitmap.width / lenna.bitmap.height) * constTwo;
+				lenna.resize(constFirst, constTwo)
+				    .quality(60)
+				    .greyscale()
+				  	.contrast(1)
+				    .getBase64(Jimp.MIME_JPEG, function (err, src) {
+				        imgXInput.value = parseInt(constFirst);
+				        imgYInput.value = constTwo;
+				        imgFile.style['width'] = (constFirst * 2).toString() + 'px';
+				        imgFile.style['height'] = (constTwo * 2).toString() + 'px';
+				        imgFile.src = src;
+
+				    for(yPixelCounter = 0; yPixelCounter < constTwo; yPixelCounter++){
+				    	for(xPixelCounter = 0; xPixelCounter < constFirst; xPixelCounter++){
+				    		if(lenna.getPixelColor(xPixelCounter, yPixelCounter) != '0x000000FF'){
+				    			imagePen.push([xPixelCounter, yPixelCounter]);
+				    		}
+				    	}
+				    }
+			    });
+			}).catch(function (err) {
+				console.error(err);
+			});			
+			document.getElementById('tool-background').style['display'] = 'none';
+		});
+
+		toolMenuConfig.appendChild(imgFile);
+		toolMenuConfig.appendChild(document.createElement('br'));
+		toolMenuConfig.appendChild(imgsDiv);
+		toolMenuConfig.appendChild(imgButton);
+		componentHandler.upgradeDom();
 	});
 });
 
@@ -160,23 +333,56 @@ function Screen(){
 					if(tool == 'write-pen'){
 						var values = this.value;
 						
-						view.addString(values, fontChoose);	
+						view.addString(values, wordWritePen);	
 
-						for(fcCounter = 0; fcCounter < fontChoose.length; fcCounter++){
+						for(fcCounter = 0; fcCounter < wordWritePen.length; fcCounter++){
 							var maxXCounter = 0;
 
-							for(fCounter = 0; fCounter < font[fontChoose[fcCounter]].length; fCounter++){
-								var pixel = (values[0] + font[fontChoose[fcCounter]][fCounter][0]).toString() + (values[1] - font[fontChoose[fcCounter]][fCounter][1]).toString() + "#";
-								if(maxXCounter < font[fontChoose[fcCounter]][fCounter][0]){
-									maxXCounter = font[fontChoose[fcCounter]][fCounter][0];
+							for(fCounter = 0; fCounter < font[wordWritePen[fcCounter]].length; fCounter++){
+			        			var idX = (values[0] + font[wordWritePen[fcCounter]][fCounter][0]).toString();
+								var idY = (values[1] - font[wordWritePen[fcCounter]][fCounter][1]).toString();
+
+								if(idX.length == 1){
+									idX = "0" + idX;
 								}
-								if(font[fontChoose[fCounter]] == ''){
+
+								if(idY.length == 1){
+									idY = "0" + idY;
+								}
+
+								var pixel = idX + idY + "#";
+								if(maxXCounter < font[wordWritePen[fcCounter]][fCounter][0]){
+									maxXCounter = font[wordWritePen[fcCounter]][fCounter][0];
+								}
+								if(font[wordWritePen[fCounter]] == ''){
 									maxXCounter = 3;
-								}
-								document.getElementById(pixel).style['background-color'] = "#87CEFA";
+								}						
+								document.getElementById(pixel).style['background-color'] = "#87CEFA";							
 							}
 							values[0] = values[0] + maxXCounter + 2;
 						}									
+					}
+
+					if(tool == 'image-pen'){
+						var values = this.value;
+
+						for(imageCounter = 0; imageCounter < imagePen.length; imageCounter++){
+							var xPixel = values[0] + imagePen[imageCounter][0];
+							var yPixel = values[1] + imagePen[imageCounter][1];
+
+							view.addPixel([xPixel, yPixel]);
+
+							if(xPixel.toString().length == 1){
+								xPixel = "0" + xPixel;
+							}
+
+							if(yPixel.toString().length == 1){
+								yPixel = "0" + yPixel;
+							}
+
+							var pixel = xPixel.toString() + yPixel.toString() + '#';
+							document.getElementById(pixel).style['background-color'] = "#87CEFA";
+						}
 					}
 				});
 
